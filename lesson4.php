b050012f29566790ba0f01b8ed72cc320827d2cf
krisb <kaabugasto@gmail.com>
2023-10-05 00:01:56 +0800

    variable types

---
+++ lesson4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    $int_var = 101;
+    $another_int = 11 + 11;
+    $num = -11;
+    
+?>
+
+</body>
+</html>
