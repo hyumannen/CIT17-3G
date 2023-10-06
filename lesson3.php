3987517b6357ee34de52481c0f6077d6c1d33d65
krisb <kaabugasto@gmail.com>
2023-10-06 23:16:36 +0800

    environment setup

--- lesson3.php
+++ lesson3.php
@@ -19,8 +19,39 @@
      I'm just trying things again
      Don't mind 
     */
+ 
+    print "Insane in 
+    the membrane."
 
 ?>
+
+<?php
+    $four = 2 + 2;//single spaces
+    $four   =   2   +   2 ;//spaces and tabs
+    $four =
+    2+
+    2;// multiple lines
+    ?>
+
+    <?php 
+    $capital = 67;
+    print("Variable capital is $capital<br>");
+    print("Variable capital is $CaPiTaL<br>");
+
+    $greeting = "Welcome to PHP!";
+    ?>
+
+   <?php
+   if(3 == 2 + 1)
+    print("It's frfr ngl ngl no cap.<br>");
+
+    if(3 == 2 + 1)
+    {
+        print("It's frfr ngl");
+        print("ngl no cap.<br>");
+    }
+   ?> 
+
 
 </body>
 </html>
