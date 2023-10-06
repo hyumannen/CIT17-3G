449805990f616c4b087ba37df1a5beaebd2bb822
krisb <kaabugasto@gmail.com>
2023-10-06 23:16:36 +0800

    syntax overview

---
+++ lesson2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Are you frfr";?>
+    <?php echo "Ngl ngl no cap "?>
+</body>
+</html>
